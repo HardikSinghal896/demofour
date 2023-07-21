d5212469002263a3350cecbb2eb8ea26baf933b2
Hardik Singhal <87109592+HardikSinghal896@users.noreply.github.com>
2023-07-21 23:25:03 +0530

    adsf

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './styles/App.css';
+// import './styles/App.scss'
+// import Header from './components/Header';
+import Home from './components/Home';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Home/>
+      
+    </>
+  );
+}
+
+export default App;
